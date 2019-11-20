2e89be9c9e6519b99a9093bf1248689456d5b3f5
EC2 Default User <ec2-user@ip-10-10-2-146.us-east-2.compute.internal>
2019-11-20 18:10:17 +0000

    updated external-dns-deploy.sh

--- k8s_services/external-dns-deploy.sh
+++ k8s_services/external-dns-deploy.sh
@@ -8,7 +8,7 @@ aws iam attach-role-policy --role-name ${rolename} --policy-arn ${route53fullacc
 
 echo "Deploy external DNS Service"
 dns_domain=$(aws route53 list-hosted-zones | egrep -o ".*builder[0-9]+.fsi405.jyidiego.net" | awk -F \" '{print $4}')
-sed 's@external-dns-test.my-org.com@'"$dns_domain"'@' external_dns/external-dns.yaml | kubectl -f -
+sed 's@external-dns-test.my-org.com@'"$dns_domain"'@' external_dns/external-dns.yaml | kubectl apply -f -
 
 echo "Checking logs.....CTRL C when you see that it is working"
 kubectl logs -n kube-system $(kubectl get po -n kube-system | egrep -o "external-dns[a-zA-Z0-9-]+") -f 
