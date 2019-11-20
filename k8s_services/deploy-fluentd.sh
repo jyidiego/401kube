8464d102790a6c8daf1f4a5d50610b51b2ea323b
EC2 Default User <ec2-user@ip-10-10-2-146.us-east-2.compute.internal>
2019-11-20 15:40:46 +0000

    added deploy-fluentd.sh

--- k8s_services/deploy-fluentd.sh
+++ k8s_services/deploy-fluentd.sh
@@ -1,4 +1,17 @@
 #!/bin/env bash
 
+clustername=fsi405-eks
+region=us-east-2
+fluentd=cwagent-fluentd.yaml
+cloudwatchlogsfull=arn:aws:iam::aws:policy/CloudWatchLogsFullAccess
+cloudwatchfull=arn:aws:iam::aws:policy/CloudWatchFullAccess
+rolename=$(aws iam list-roles --output text | grep EksStack-fsi405eksDefaultCapacityInstanceRole | awk -F: '{print $6}' | awk '{print $1}' | awk -F/ '{print $2}')
 
-echo "hello world"
+echo "Attaching CloudWatchFull and CloudWatchLogFullAccess policies to EKS worker node role"
+aws iam attach-role-policy --role-name ${rolename} --policy-arn ${cloudwatchlogsfull}
+aws iam attach-role-policy --role-name ${rolename} --policy-arn ${cloudwatchfull}
+
+echo "Deploying fluentd to EKS"
+sed 's/{{cluster_name}}/fsi405-eks/;s/{{region_name}}/us-east-2/' ./fluentd/${fluentd} | kubectl apply -f -
+# kubectl logs -n amazon-cloudwatch $(kubectl get po -n amazon-cloudwatch | egrep -o "fluentd-cloudwatch[a-zA-Z0-9-]+") -f
+echo $cloudwatchlogsfull
