65df3c04f1465196e905dcd8a65fe13fd2b48d93
John Yi <johnyi@Johns-MacBook-Pro.local>
2019-12-12 08:53:55 -0500

    added eksctl installation

--- install_kubectl.sh
+++ install_kubectl.sh
@@ -28,3 +28,9 @@ aws-iam-authenticator help
 
 echo "sudo yum install jq -y"
 sudo yum install jq -y
+
+echo "Downloading eksctl"
+curl --silent --location "https://github.com/weaveworks/eksctl/releases/download/latest_release/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
+
+echo "Installing eksctl..."
+sudo mv /tmp/eksctl /usr/local/bin
